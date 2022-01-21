8728ccb5d1e94c262b3dbeabd9bb2f69d5bf55ab
John Helly <j.c.helly@durham.ac.uk>
2022-01-21 10:04:10 +0000

    Added batch script to combine L1000N1800 runs

---
+++ scripts/FLAMINGO/combine_L1000N1800.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#
+#SBATCH --nodes=1
+#SBATCH --tasks-per-node=70
+#SBATCH --cpus-per-task=1
+#SBATCH -o ./logs/L1000N1800/%x.out
+#SBATCH -p cosma8
+#SBATCH -A dp004
+#SBATCH -t 72:00:00
+#
+
+module purge
+module load gnu_comp/11.1.0 openmpi/4.1.1
+module load python/3.10.1
+
+sim="L1000N1800/${SLURM_JOB_NAME}"
+
+input_dir=/cosma8/data/dp004/jlvc76/FLAMINGO/ScienceRuns/${sim}/lightcones/
+output_dir=/cosma8/data/dp004/jch/FLAMINGO/ScienceRuns/${sim}/lightcones/
+
+nr_lightcones=2
+
+mpirun python3 -m mpi4py \
+    ~/Codes/Flamingo/combine_healpix/combine_maps_mpi.py \
+    ${input_dir} ${nr_lightcones} ${output_dir}  
