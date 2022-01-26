3297397dc5c553ed2c6d3345a2a25a476e3d411a
John Helly <j.c.helly@durham.ac.uk>
2022-01-26 10:29:28 +0000

    Remember to write out the result in make_map_from_particles.py

--- examples/make_map_from_particles.py
+++ examples/make_map_from_particles.py
@@ -9,10 +9,13 @@ import numpy as np
 import lightcone_io.particle_reader as pr
 
 # Specify one file from the spatially indexed lightcone
-filename = "/cosma8/data/dp004/jch/FLAMINGO/ScienceRuns/L1000N1800/DMO_FIDUCIAL/lightcones/lightcone0_particles/lightcone0_0000.0.hdf5"
+input_filename = "/cosma8/data/dp004/jch/FLAMINGO/ScienceRuns/L1000N1800/DMO_FIDUCIAL/lightcones/lightcone0_particles/lightcone0_0000.0.hdf5"
+
+# Where to write the result
+output_filename = "/cosma8/data/dp004/jch/lightcone_map.hdf5"
 
 # Open the lightcone
-lightcone = pr.IndexedLightcone(filename)
+lightcone = pr.IndexedLightcone(input_filename)
 
 # Create an empty HEALPix map
 nside = 16384
@@ -28,6 +31,7 @@ redshift_range = (0.0, 0.05)
 # Find out how many particles there will be
 nr_particles_tot = lightcone["DM"].count_particles(redshift_range=redshift_range)
 nr_particles_done = 0
+print("Total particles to read = %d" % nr_particles_tot)
 
 # Loop over particles in the lightcone
 for particle_chunk in lightcone["DM"].iterate_chunks(property_names=property_names,
@@ -48,15 +52,19 @@ for particle_chunk in lightcone["DM"].iterate_chunks(property_names=property_nam
     del z
 
     # Determine which pixels these particles belong to
-    pos = pos.value     # vec2pix can't handle unyt arrays
+    pos = pos.value # vec2pix can't handle unyt arrays, and we only need the direction from the vector anyway
     pixel = hp.pixelfunc.vec2pix(nside, pos[:,0], pos[:,1], pos[:,2])
     
     # Accumulate pixels to the map
-    np.add.at(map_data, pixel, m.value)
-
-    # This way is much slower because it updates all pixels on each iteration
+    # This way is much slower because it updates all pixels in the map on each iteration
     #map_data += np.bincount(pixel, minlength=npix, weights=m)
+    # Faster to use np.add.at to only update the affected pixels
+    np.add.at(map_data, pixel, m.value)
 
     # Report progress
     nr_particles_done += pos.shape[0]
     print("Percentage of particles processed = %.2f" % ((nr_particles_done/nr_particles_tot)*100.0))
+
+# Write out the result
+with h5py.File(output_filename, "w") as outfile:
+    outfile["MassMap"] = map_data
