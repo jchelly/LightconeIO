91002545c855b833740f69ec4a2172db8953f9a3
John Helly <j.c.helly@durham.ac.uk>
2022-02-14 11:37:22 +0000

    Fix check on no. of args in combine_maps script

--- bin/lightcone_io_combine_maps.py
+++ bin/lightcone_io_combine_maps.py
@@ -59,7 +59,7 @@ if __name__ == "__main__":
 
     args = {}
     if comm_rank == 0:
-        if len(sys.argv) != 4:
+        if len(sys.argv) < 4:
             print(usage)
             args = None
         else:
