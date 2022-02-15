36d46c77d1e538d97c3eeac6af238928f224627a
John Helly <j.c.helly@durham.ac.uk>
2022-02-15 15:48:48 +0000

    Add batch script to downsample L1000N1800 maps

---
+++ scripts/FLAMINGO/downsample_L1000N1800.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#
+#SBATCH --nodes=1
+#SBATCH --tasks-per-node=10
+#SBATCH --cpus-per-task=1
+#SBATCH -o ./logs/L1000N1800/downsample_%x.lightcone%a.out
+#SBATCH -p cosma8
+#SBATCH -A dp004
+#SBATCH -t 72:00:00
+#
+
+new_nside=1024
+
+module purge
+module load gnu_comp/11.1.0 openmpi/4.1.1
+module load python/3.10.1
+
+sim="L1000N1800/${SLURM_JOB_NAME}"
+lightcone_nr=${SLURM_ARRAY_TASK_ID}
+basename=lightcone${lightcone_nr}
+
+input_dir=/cosma8/data/dp004/jch/FLAMINGO/ScienceRuns/${sim}/lightcones/
+output_dir=/cosma8/data/dp004/jch/FLAMINGO/ScienceRuns/${sim}/lightcones_downsampled/
+
+# Assume script is in $PATH
+script=`which lightcone_io_downsample_maps.py`
+
+mpirun python3 -m mpi4py ${script} ${input_dir} ${output_dir} ${basename} ${new_nside}
