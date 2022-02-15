df8f38b303f60f2d80b6013273edd70b22c2d681
John Helly <j.c.helly@durham.ac.uk>
2022-02-15 11:51:07 +0000

    More log output in lightcone_io_index_particles.py

--- bin/lightcone_io_index_particles.py
+++ bin/lightcone_io_index_particles.py
@@ -31,15 +31,18 @@ if __name__ == "__main__":
             args["nr_redshift_bins"] = int(sys.argv[3])
             args["nside"]            = int(sys.argv[4])
             args["outdir"]           = sys.argv[5]
+
     args = comm.bcast(args)
-
     if args is None:
-        MPI.Finalize()
-        sys.exit(0)
+        comm.abort()
 
+    if comm_rank == 0:
+        print("Reading metadata")
     lightcone = LightconeSorter(args["basedir"], args["basename"],
                                 MPI.COMM_WORLD)
-    
+
+    if comm_rank == 0:
+        print("Sorting lightcone")    
     lightcone.write_sorted_lightcone(args["outdir"], args["nr_redshift_bins"],
                                      args["nside"])
 
