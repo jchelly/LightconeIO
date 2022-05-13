d38d04d96b6bf23554c8f528ffd442645a0becf4
John Helly <j.c.helly@durham.ac.uk>
2022-05-13 11:01:02 +0100

    Add class to evaluate projected kernel

--- lightcone_io/kernel.py
+++ lightcone_io/kernel.py
@@ -1,6 +1,8 @@
 #!/bin/env python
 
 import numpy as np
+import scipy.integrate as integrate
+import scipy.interpolate as interpolate
 
 #
 # Wendland C2 kernel as used in FLAMINGO
@@ -22,23 +24,69 @@ kernel_norm=(4./3*np.pi) * kernel_gamma_dim
 
 
 def kernel_eval(u):
+    """
+    Numpy vectorized kernel function
+    """
+    u = np.asarray(u, dtype=float)
 
     # Go to the range [0,1[ from [0,H[
     x = u * kernel_gamma_inv
     
     # Pick the correct branch of the kernel
-    temp = int(x * kernel_ivals_f)
-    ind = kernel_ivals if temp > kernel_ivals else temp
-    coeffs = kernel_coeffs[ind * (kernel_degree + 1):]
+    temp = (x * kernel_ivals_f).astype(int)
+    ind = np.where(temp > kernel_ivals, kernel_ivals, temp)
+
+    coeffs_index = ind * (kernel_degree + 1)
 
     # First two terms of the polynomial ...
-    w = coeffs[0] * x + coeffs[1]
+    w = kernel_coeffs[coeffs_index] * x + kernel_coeffs[coeffs_index+1]
 
     # ... and the rest of them
     for k in range(2, kernel_degree+1):
-        w = x * w + coeffs[k]
+        w = x * w + kernel_coeffs[coeffs_index+k]
 
-    w = max(w, 0.)
+    w = np.asarray(w)
+    w[w < 0.0] = 0.0
 
     return w * kernel_constant * kernel_gamma_inv_dim
 
+
+def projected_kernel_integrate(u):
+    
+    u = float(u) # not implemented for array input
+
+    # Avoid integration errors if u is out of range
+    if u > kernel_gamma:
+        return 0.0
+
+    projected_kernel_integrand = lambda qz : kernel_eval(np.sqrt(u**2+qz**2))
+
+    qz_max = np.sqrt(kernel_gamma**2-u**2)
+    qz_min = -qz_max
+
+    result, abserror = integrate.quad(projected_kernel_integrand, qz_min, qz_max)
+    return result
+
+
+class ProjectedKernel:
+    """
+    Evaluate the projected kernel by interpolating a tabulated function
+    """
+    def __init__(self):
+        """Tabulate the projected kernel"""
+        npoints = 1000
+        x = np.linspace(0.0, kernel_gamma, npoints)
+        y = np.zeros_like(x)
+        for i in range(npoints):
+            y[i] = projected_kernel_integrate(x[i])
+        self.interp = interpolate.interp1d(x, y)
+
+    def __call__(self, u):
+        """Evaluate the projected kernel at the input radii"""
+        u = np.asarray(u)
+        if np.any(u < 0.0):
+            raise ValueError("Can't evaluate projected kernel at negative radius!")
+        in_range = u < kernel_gamma
+        result = np.zeros_like(u, dtype=float)
+        result[in_range] = self.interp(u[in_range])
+        return result
