c0eaf2250326a8eaa22d90ee045747e528b21c36
John Helly <j.c.helly@durham.ac.uk>
2022-06-20 11:59:48 +0100

    Fixes to L1000N1800 smoothed gas map example

--- examples/make_smoothed_map.py
+++ examples/make_smoothed_map.py
@@ -1,81 +1,49 @@
 #!/bin/env python
 
+import sys
+
 import h5py
+import numpy as np
 import lightcone_io.smoothed_map as smoothed_map
 import virgo.mpi.parallel_hdf5 as phdf5
 
 from mpi4py import MPI
 comm = MPI.COMM_WORLD
+comm_rank = comm.Get_rank()
 
 
-def test_bh_map():
-
-    nside = 16384
-    input_filename = "/cosma8/data/dp004/jch/FLAMINGO/BlackHoles/200_w_lightcone/sorted_lightcones_new/lightcone0_particles/lightcone0_0000.0.hdf5"
-    output_filename = "/cosma8/data/dp004/jch/lightcone_map.hdf5"
-    zmin, zmax = (0.05, 0.1)
-    ptype = "BH"
-    property_names = ("DynamicalMasses",)
-
-    # Function to return the quantity to map, given the lightcone particle data dict
-    def particle_mass(particle_data):
-        return particle_data["DynamicalMasses"]
-
-    dataset_name = "BlackHoleMass"
-
-    # Make the map
-    map_data = smoothed_map.make_sky_map(input_filename, ptype, property_names, particle_mass,
-                                         zmin, zmax, nside, smooth=False)
-
-    # Write out the new map
-    with h5py.File(output_filename, "w", driver="mpio", comm=comm) as outfile:
-        phdf5.collective_write(outfile, dataset_name, map_data, comm)
-
-
-def test_gas_map():
-
-    nside = 16384
-    input_filename = "/cosma8/data/dp004/jch/FLAMINGO/BlackHoles/200_w_lightcone/sorted_lightcones_new/lightcone0_particles/lightcone0_0000.0.hdf5"
-    output_filename = "/cosma8/data/dp004/jch/lightcone_map.hdf5"
-    zmin, zmax = (0.05, 0.1)
-    ptype = "Gas"
-    property_names = ("Masses",)
-
-    # Function to return the quantity to map, given the lightcone particle data dict
-    def particle_mass(particle_data):
-        return particle_data["Masses"]
-
-    dataset_name = "SmoothedGasMass"
-
-    # Make the map
-    map_data = smoothed_map.make_sky_map(input_filename, ptype, property_names, particle_mass,
-                                         zmin, zmax, nside, smooth=True)
-
-    # Write out the new map
-    with h5py.File(output_filename, "w", driver="mpio", comm=comm) as outfile:
-        phdf5.collective_write(outfile, dataset_name, map_data, comm)
-
-
-def L1000N1800_HYDRO_FIDUCIAL_smoothed_gas_mass():
+def L1000N1800_HYDRO_FIDUCIAL_smoothed_gas_mass(shell_nr):
 
     nside = 16384
     input_filename = "/cosma8/data/dp004/jch/FLAMINGO/ScienceRuns/L1000N1800/HYDRO_FIDUCIAL/lightcones/lightcone0_particles/lightcone0_0000.0.hdf5"
-    output_filename = "/cosma8/data/dp004/jch/FLAMINGO/ScienceRuns/L1000N1800/HYDRO_FIDUCIAL/new_maps/smoothed_gas_map.hdf5"
-    zmin, zmax = (0.05, 0.1)
+    output_filename = f"/cosma8/data/dp004/jch/FLAMINGO/ScienceRuns/L1000N1800/HYDRO_FIDUCIAL/new_maps/smoothed_gas_map.shell_{shell_nr}.hdf5"
+    shell_redshifts = "/cosma8/data/dp004/flamingo/Runs/L1000N1800/HYDRO_FIDUCIAL/shell_redshifts_z3.txt"
+
+    # Read shell redshifts
+    if comm_rank == 0:
+        redshifts = np.loadtxt(shell_redshifts, delimiter=",")
+        zmin = redshifts[shell_nr,0]
+        zmax = redshifts[shell_nr,1]
+        print(f"Reproducing shell {shell_nr} with zmin={zmin} and zmax={zmax}")
+    else:
+        zmin = None
+        zmax = None
+    zmin, zmax = comm.bcast((zmin, zmax))
+
+    # Specify which particle types and properties to read in
     ptype = "Gas"
     property_names = ("Masses",)
 
     # Function to return the quantity to map, given the lightcone particle data dict
     def particle_mass(particle_data):
         return particle_data["Masses"]
-
-    dataset_name = "SmoothedGasMass"
 
     # Make the map
     map_data = smoothed_map.make_sky_map(input_filename, ptype, property_names, particle_mass,
                                          zmin, zmax, nside, smooth=True, progress=True)
 
     # Write out the new map
+    dataset_name = "SmoothedGasMass"
     with h5py.File(output_filename, "w", driver="mpio", comm=comm) as outfile:
         phdf5.collective_write(outfile, dataset_name, map_data, comm)
 
@@ -83,5 +51,11 @@ def L1000N1800_HYDRO_FIDUCIAL_smoothed_gas_mass():
 
 if __name__ == "__main__":
 
-    L1000N1800_HYDRO_FIDUCIAL_smoothed_gas_mass()
-    #test_bh_map()
+    # Shell to do should be specified on the command line
+    if comm_rank == 0:
+        shell_nr = int(sys.argv[1])
+    else:
+        shell_nr = None
+    shell_nr = comm.bcast(shell_nr)
+
+    L1000N1800_HYDRO_FIDUCIAL_smoothed_gas_mass(shell_nr)
--- examples/make_smoothed_map.sh
+++ examples/make_smoothed_map.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #
-#SBATCH --nodes=4
+#SBATCH --nodes=2
 #SBATCH --tasks-per-node=128
 #SBATCH --cpus-per-task=1
-#SBATCH -o ./test.out
+#SBATCH -o ./logs/smoothed_map.%A.%a.out
 #SBATCH -p cosma8
 #SBATCH -A dp004
 #SBATCH -t 72:00:00
@@ -13,5 +13,4 @@ module purge
 module load gnu_comp/11.1.0 openmpi/4.1.1
 module load python/3.10.1
 
-mpirun python3 -m mpi4py ./smoothed_full_sky_map_from_particles.py
-
+mpirun python3 -m mpi4py ./make_smoothed_map.py ${SLURM_ARRAY_TASK_ID}
