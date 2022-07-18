827fcc95c29724958f043eb824fc5210ae4acb15
John Helly <j.c.helly@durham.ac.uk>
2022-07-18 15:08:36 +0100

    Downsample corrected maps to nside=4096 for L1000N1800 runs

--- scripts/FLAMINGO/downsample_L1000N1800.sh
+++ scripts/FLAMINGO/downsample_L1000N1800.sh
@@ -3,13 +3,13 @@
 #SBATCH --nodes=1
 #SBATCH --tasks-per-node=10
 #SBATCH --cpus-per-task=1
-#SBATCH -o ./logs/L1000N1800/downsample_%x.lightcone%a.out
+#SBATCH -o ./logs/L1000N1800/downsample4096_%x.lightcone%a.out
 #SBATCH -p cosma8
 #SBATCH -A dp004
 #SBATCH -t 72:00:00
 #
 
-new_nside=1024
+new_nside=4096
 
 module purge
 module load gnu_comp/11.1.0 openmpi/4.1.1
@@ -19,8 +19,14 @@ sim="L1000N1800/${SLURM_JOB_NAME}"
 lightcone_nr=${SLURM_ARRAY_TASK_ID}
 basename=lightcone${lightcone_nr}
 
-input_dir=/cosma8/data/dp004/jch/FLAMINGO/ScienceRuns/${sim}/lightcones/
-output_dir=/cosma8/data/dp004/jch/FLAMINGO/ScienceRuns/${sim}/lightcones_downsampled/
+if [[ $SLURM_JOB_NAME == DMO_* ]] ; then
+  # DMO maps don't need to be corrected
+  input_dir=/cosma8/data/dp004/jch/FLAMINGO/ScienceRuns/${sim}/lightcones/
+  output_dir=/cosma8/data/dp004/jch/FLAMINGO/ScienceRuns/${sim}/lightcones_downsampled_${new_nside}/
+else
+  input_dir=/cosma8/data/dp004/jch/FLAMINGO/ScienceRuns/${sim}/corrected_maps/
+  output_dir=/cosma8/data/dp004/jch/FLAMINGO/ScienceRuns/${sim}/corrected_downsampled_${new_nside}/
+fi
 
 # Assume script is in $PATH
 script=`which lightcone_io_downsample_maps.py`
