6497595eea364914ee76c287214b146da674efab
John Helly <j.c.helly@durham.ac.uk>
2022-11-01 14:39:13 +0000

    Add neutrino mass map corrections

--- bin/lightcone_io_correct_maps.py
+++ bin/lightcone_io_correct_maps.py
@@ -15,7 +15,7 @@ comm_rank = comm.Get_rank()
 comm_size = comm.Get_size()
 
 
-def correct_shell_file(indir, basename, outdir, shell_nr, z_central):
+def correct_shell_file(indir, basename, outdir, shell_nr, z_central, neutrino_density):
 
     # Generate shell input and output file names
     infile = ("%s/%s_shells/shell_%d/%s.shell_%d.0.hdf5" %
@@ -44,42 +44,74 @@ def correct_shell_file(indir, basename, outdir, shell_nr, z_central):
     # DopplerB needs to be multiplied by (1+z)
     if "DopplerB" in f:
         dset = f["DopplerB"]
-        if "Correction factor" in dset.attrs:
-            raise Exception("Dataset has already been corrected!")
-        fac = (1.0+z_central[shell_nr])
-        dset.attrs["Correction applied"] = "Multiplied by 1+(z_min+z_max)/2 to compensate for bug in original output"
-        dset.attrs["Correction factor"] = fac
-        dset.attrs["Central redshift assumed for correction"] = z_central[shell_nr]
-        nchunk=100*1024*1024
-        ntot = dset.shape[0]
-        for offset in range(0, ntot, nchunk):
-            n = min(nchunk, ntot-offset)
-            data = dset[offset:offset+n,...]
-            data *= fac
-            dset[offset:offset+n,...] = data
+        if "Correction factor" not in dset.attrs:
+            fac = (1.0+z_central[shell_nr])
+            dset.attrs["Correction applied"] = "Multiplied by 1+(z_min+z_max)/2 to compensate for bug in original output"
+            dset.attrs["Correction factor"] = fac
+            dset.attrs["Central redshift assumed for correction"] = z_central[shell_nr]
+            nchunk=100*1024*1024
+            ntot = dset.shape[0]
+            for offset in range(0, ntot, nchunk):
+                n = min(nchunk, ntot-offset)
+                data = dset[offset:offset+n,...]
+                data *= fac
+                dset[offset:offset+n,...] = data
+        else:
+            print(f"  Skipping DopplerB correction for shell {shell_nr} of {basename}")
 
     # DM needs to be multiplied by 1/(1+z)
     if "DM" in f:
         dset = f["DM"]
-        if "Correction factor" in dset.attrs:
-            raise Exception("Dataset has already been corrected!")
-        fac = 1.0/(1.0+z_central[shell_nr])
-        dset.attrs["Correction applied"] = "Multiplied by 1/(1+(z_min+z_max)/2) to compensate for bug in original output"
-        dset.attrs["Correction factor"] = fac
-        dset.attrs["Central redshift assumed for correction"] = z_central[shell_nr]
-        nchunk=100*1024*1024
-        ntot = dset.shape[0]
-        for offset in range(0, ntot, nchunk):
-            n = min(nchunk, ntot-offset)
-            data = dset[offset:offset+n,...]
-            data *= fac
-            dset[offset:offset+n,...] = data
+        if "Correction factor" not in dset.attrs:
+            fac = 1.0/(1.0+z_central[shell_nr])
+            dset.attrs["Correction applied"] = "Multiplied by 1/(1+(z_min+z_max)/2) to compensate for bug in original output"
+            dset.attrs["Correction factor"] = fac
+            dset.attrs["Central redshift assumed for correction"] = z_central[shell_nr]
+            nchunk=100*1024*1024
+            ntot = dset.shape[0]
+            for offset in range(0, ntot, nchunk):
+                n = min(nchunk, ntot-offset)
+                data = dset[offset:offset+n,...]
+                data *= fac
+                dset[offset:offset+n,...] = data
+        else:
+            print(f"  Skipping DM correction for shell {shell_nr} of {basename}")
+
+
+    # Neutrino mass maps:
+    # Need to subtract 3*(mean neutrino density) to get actual neutrino density.
+    # Will need to correct the neutrino mass and total mass maps. The mean
+    # neutrino density can be extracted from the snapshot file.
+    for name in ("NeutrinoMass", "TotalMass"):
+        if name in f:
+            dset = f[name]
+            if "Correction offset" not in dset.attrs:
+                # Find the volume of a pixel
+                r1 = dset.attrs["comoving_inner_radius"][0]
+                r2 = dset.attrs["comoving_outer_radius"][0]
+                npix = dset.attrs["number_of_pixels"]
+                pixel_volume = (4./3.)*np.pi*(r2**3-r1**3)/npix
+                # Compute offset to apply to the map
+                mass_per_pixel = -3.0*(neutrino_density*pixel_volume)
+                # Update attributes with correction to apply
+                dset.attrs["Correction applied"] = ["Subtracted 3 times the mean neutrino mass per pixel to fix bug in original output"]
+                dset.attrs["Correction offset"] = mass_per_pixel
+                # Apply  the correction to the neutrino mass
+                nchunk=100*1024*1024
+                ntot = dset.shape[0]
+                for offset in range(0, ntot, nchunk):
+                    n = min(nchunk, ntot-offset)
+                    data = dset[offset:offset+n,...]
+                    data += mass_per_pixel
+                    dset[offset:offset+n,...] = data
+            else:
+                print(f"  Skipping {name} correction for shell {shell_nr} of {basename}")
 
     f.close()
     print("Shell %d done" % shell_nr)
 
 
-def correct_maps_mpi(indir, yml_file, outdir, basenames):
+def correct_maps_mpi(indir, yml_file, outdir, snapfile, basenames):
 
     if comm_rank == 0:
 
@@ -89,6 +121,14 @@ def correct_maps_mpi(indir, yml_file, outdir, basenames):
         # Read simulation config file
         config = yaml.load(open(yml_file, "r"), yaml.loader.SafeLoader)
         
+        # Get mean comoving neutrino density from the snapshot
+        with h5py.File(snapfile, "r") as snap:
+            omega_nu_0 = snap["Cosmology"].attrs["Omega_nu_0"][0]
+            H0 = snap["Cosmology"].attrs["H0 [internal units]"][0]
+            G = snap["PhysicalConstants"]["InternalUnits"].attrs["newton_G"][0]
+        critical_density_0 = 3*(H0**2)/(8*np.pi*G)
+        neutrino_density = omega_nu_0 * critical_density_0
+
         # Find paths to the shell redshift lists:
         # May be a different file for each lightcone
         radius_file = {}
@@ -151,7 +191,8 @@ def correct_maps_mpi(indir, yml_file, outdir, basenames):
             shutil.copyfile(infile, outfile)
 
             # Store arguments to process this shell
-            args += [(indir, basename, outdir, shell_nr, z_central[basename]) for shell_nr in range(nr_shells)]
+            args += [(indir, basename, outdir, shell_nr, z_central[basename], neutrino_density)
+                     for shell_nr in range(nr_shells)]
 
     # Make sure all output dirs have been created
     comm.barrier()
@@ -176,21 +217,22 @@ if __name__ == "__main__":
 
     args = {}
     if comm_rank == 0:
-        if len(sys.argv) < 4:
+        if len(sys.argv) < 5:
             print(usage)
             args = None
         else:
             args["indir"] = sys.argv[1]
             args["yml_file"] = sys.argv[2]
             args["outdir"] = sys.argv[3]
-            args["basenames"] = sys.argv[4:]
+            args["snapfile"] = sys.argv[4]
+            args["basenames"] = sys.argv[5:]
     args = comm.bcast(args)
 
     if args is None:
         MPI.Finalize()
         sys.exit(0)
 
-    correct_maps_mpi(args["indir"], args["yml_file"], args["outdir"], args["basenames"])
+    correct_maps_mpi(args["indir"], args["yml_file"], args["outdir"], args["snapfile"], args["basenames"])
 
     comm.barrier()
     if comm_rank == 0:
