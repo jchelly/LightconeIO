76d79fa96c38870f52f3c52a63db7fbada0a1cd5
John Helly <j.c.helly@durham.ac.uk>
2022-11-10 10:03:21 +0000

    Update BH matching script for new trees

--- scripts/FLAMINGO/match_bh_L1000N1800.sh
+++ scripts/FLAMINGO/match_bh_L1000N1800.sh
@@ -4,7 +4,7 @@
 #SBATCH --tasks-per-node=128
 #SBATCH --cpus-per-task=1
 #SBATCH -o ./logs/L1000N1800/match_bh_%x.lightcone%a.out
-#SBATCH -p cosma8-shm2
+#SBATCH -p cosma8
 #SBATCH -A dp004
 #SBATCH -t 72:00:00
 #
@@ -17,7 +17,7 @@ sim="L1000N1800/${SLURM_JOB_NAME}"
 lightcone_nr=${SLURM_ARRAY_TASK_ID}
 
 # Location of the merger trees
-tree_basename=/cosma8/data/dp004/jch/FLAMINGO/MergerTrees/ScienceRuns/${sim}/output/Velociraptor
+tree_filename=/cosma8/data/dp004/jch/FLAMINGO/MergerTrees/ScienceRuns/${sim}/trees_f0.1_min10_max100/vr_trees.hdf5
 
 # Location of the lightcone particle data
 lightcone_dir=/cosma8/data/dp004/jch/FLAMINGO/ScienceRuns/${sim}/particle_lightcones/
@@ -31,4 +31,4 @@ lfs setstripe --stripe-count=-1 --stripe-size=32M ${output_dir}
 # Assume we're running from scripts/FLAMINGO in the source directory
 script=../../bin/lightcone_io_match_black_holes.py 
 
-mpirun python3 -m mpi4py ${script} ${tree_basename} ${lightcone_dir} ${lightcone_base} ${output_dir}
+mpirun python3 -m mpi4py ${script} ${tree_filename} ${lightcone_dir} ${lightcone_base} ${output_dir}
