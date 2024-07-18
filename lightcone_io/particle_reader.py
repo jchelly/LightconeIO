bb430751b2232a4203a807342b26ae3c4be9a6c9
John Helly <j.c.helly@durham.ac.uk>
2024-07-18 14:47:49 +0100

    Accept nr_mpi_ranks as scalar or one element array

--- lightcone_io/particle_reader.py
+++ lightcone_io/particle_reader.py
@@ -509,13 +509,13 @@ class IndexedLightcone(collections.abc.Mapping):
 
                 # Find names of all of the lightcone files
                 filenames = []
-                for i in range(metadata["nr_mpi_ranks"]):
+                for i in range(int(metadata["nr_mpi_ranks"])):
                     filenames.append("%s.%d.hdf5" % (basename, i))
 
                 # Find names of extra files
                 if extra_filename is not None:
                     extra_filenames = []
-                    for i in range(metadata["nr_mpi_ranks"]):
+                    for i in range(int(metadata["nr_mpi_ranks"])):
                         extra_filenames.append("%s.%d.hdf5" % (extra_basename, i))
                 else:
                     extra_filenames = None
