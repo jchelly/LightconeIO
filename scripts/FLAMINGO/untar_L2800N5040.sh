d206f2d3b1cc050a7433367da730d90373204322
John Helly <j.c.helly@durham.ac.uk>
2024-07-23 12:46:27 +0100

    Fix mistake in 2.8Gpc unpacking script

--- scripts/FLAMINGO/untar_L2800N5040.sh
+++ scripts/FLAMINGO/untar_L2800N5040.sh
@@ -18,7 +18,8 @@ module load gnu_comp/14.1.0 openmpi
 srcdir="/cosma8/data/dp004/flamingo/Runs/L2800N5040/${name}/lightcones/lightcone${lightcone_nr}_particles/"
 destdir="/snap8/scratch/dp004/jch/FLAMINGO/ScienceRuns/L2800N5040/${name}/"
 
-\mkdir -p ${destdir}
+\mkdir -p "${destdir}"
+lfs setstripe --stripe-count=1 "${destdir}"
 
-cd ${destdir} && mpirun -np 20 /cosma/home/dp004/jch/Codes/parallel_tasks/build/parallel_tasks 0 80 \
-    "tar xvfz ${srcdir}/lightcone0_%04d.tar.gz"
+cd "${destdir}" && mpirun -np 20 /cosma/home/dp004/jch/Codes/parallel_tasks/build/parallel_tasks 0 80 \
+    "tar xvfz ${srcdir}/lightcone${lightcone_nr}_%04d.tar.gz"
