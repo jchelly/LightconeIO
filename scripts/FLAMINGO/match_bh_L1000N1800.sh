3925df19b3307c54bae511f4a47295d329beadf2
John Helly <j.c.helly@durham.ac.uk>
2024-08-08 12:24:58 +0100

    Update BH matching scripts

--- scripts/FLAMINGO/match_bh_L1000N1800.sh
+++ scripts/FLAMINGO/match_bh_L1000N1800.sh
@@ -1,19 +1,19 @@
-#!/bin/bash
+#!/bin/bash -l
 #
 #SBATCH --nodes=1
 #SBATCH --tasks-per-node=128
 #SBATCH --cpus-per-task=1
-#SBATCH -o ./logs/L1000N1800/match_bh_%x.lightcone%a.out
+#SBATCH -o ./logs/L1000N1800/match_bh_%x.lightcone%a.%A.out
 #SBATCH -p cosma8
 #SBATCH -A dp004
-#SBATCH -t 3:00:00
+#SBATCH -t 4:00:00
 #
 
 module purge
 module load gnu_comp/14.1.0 openmpi/5.0.3
 module load python/3.12.4
 
-activate lightcone_io
+source /cosma/apps/dp004/${USER}/lightcone_env/bin/activate
 
 # Simulation to do (based on job name)
 sim="L1000N1800/${SLURM_JOB_NAME}"
@@ -35,11 +35,12 @@ snapshot_format="/cosma8/data/dp004/flamingo/Runs/${sim}/snapshots/flamingo_{sna
 membership_format="/cosma8/data/dp004/flamingo/Runs/${sim}/SOAP-HBT/membership_{snap_nr:04d}/membership_{snap_nr:04d}.{file_nr}.hdf5"
 
 # Where to write the output
-output_dir=/snap8/scratch/dp004/jch/FLAMINGO/Tests/${sim}/lightcone_halos/lightcone${lightcone_nr}/
+output_dir=/cosma8/data/dp004/${USER}/FLAMINGO/HBT/${sim}/lightcone_halos/lightcone${lightcone_nr}/
 \mkdir -p ${output_dir}
 lfs setstripe --stripe-count=-1 --stripe-size=32M ${output_dir}
 
 # Run
-mpirun python3 -m mpi4py -m lightcone_io.match_black_holes \
-       "${halo_format}" 0 77 75 77 "${lightcone_dir}" "lightcone${lightcone_nr}" "${snapshot_format}" "${membership_format}" "${output_dir}" \
-       --halo-type=HBTplus
+mpirun -- python3 -m mpi4py -m lightcone_io.match_black_holes \
+       "${halo_format}" 0 77 5 36 "${lightcone_dir}" "lightcone${lightcone_nr}" "${snapshot_format}" "${membership_format}" "${output_dir}" \
+       --halo-type=HBTplus \
+       --pass-through="InputHalos/IsCentral,InputHalos/NumberOfBoundParticles,BoundSubhalo/TotalMass,InputHalos/HBTplus/TrackId"
