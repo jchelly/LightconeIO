4e40caed08c3fefa15609c1539101ca5c3dee8b6
John Helly <j.c.helly@durham.ac.uk>
2025-10-10 16:22:54 +0100

    Import main classes into top level module

--- lightcone_io/__init__.py
+++ lightcone_io/__init__.py
@@ -1,1 +1,8 @@
-__all__ = []
+__all__ = ["Shell", "ShellArray", "HealpixMap", "LightconeParticleType", "LightconeParticles"]
+
+# Classes for reading lightcone particle data
+from .particle_reader import IndexedLightconeParticleType as LightconeParticleType
+from .particle_reader import IndexedLightcone as LightconeParticles
+
+# Classes for reading healpix maps
+from .healpix_maps import Shell, ShellArray, HealpixMap
