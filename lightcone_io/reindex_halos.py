d8318b2209b2823a71fb69811ff5de71f0c1ecb9
John Helly <j.c.helly@durham.ac.uk>
2025-12-08 15:08:46 +0000

    Fix attribute names in new dataset

--- lightcone_io/reindex_halos.py
+++ lightcone_io/reindex_halos.py
@@ -134,7 +134,7 @@ def reindex_halos(snap_nr, input_lightcone_dir, lightcone_base,
             dataset.attrs["Conversion factor to CGS (not including cosmological corrections"] = [1.0]
             dataset.attrs["Conversion factor to CGS (including cosmological corrections"] = [1.0]
             for dim in ("U_I", "U_L", "U_M", "U_T", "U_t", "a-scale", "h-scale"):
-                dataset.attrs[f"U_{dim} exponent"] = [0.0]
+                dataset.attrs[f"{dim} exponent"] = [0.0]
         property_names.append("InputHalos/SOAPIndex")
 
     comm.barrier()
