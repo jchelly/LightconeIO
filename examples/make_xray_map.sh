107f13ec8dcc5b68ab1e989cda389798de2e5801
Will-McD <77323079+Will-McD@users.noreply.github.com>
2026-03-18 16:26:15 +0100

    Create make_xray_map.sh
    bash script for x-ray map example

---
+++ examples/make_xray_map.sh
@@ -0,0 +1,45 @@
+#!/bin/bash -l
+#SBATCH --array=5 # shell number
+#SBATCH --nodes=1
+#SBATCH --tasks-per-node=32
+#SBATCH --cpus-per-task=1
+#SBATCH -J HYDRO_FIDUCIAL
+#SBATCH -o ./logs/smoothed_xray_map.%x.%a.out              
+#SBATCH -e ./logs/smoothed_xray_map.%x.%a.err
+#SBATCH -p cosma8
+#SBATCH -A dp203
+#SBATCH -t 03:00:00
+
+
+module purge
+module load gnu_comp/14.1.0 openmpi/5.0.3
+module load python/3.12.4
+
+simulation_LN="L1000N1800"
+simulation_name="${SLURM_JOB_NAME}"
+
+# replace with own lightcone_io virtual env
+source /cosma/apps/dp004/${USER}/lightcone_env/bin/activate
+
+# lightcone (or observer) number 
+lightcone_nr=0 
+
+# HEALPix map resolution
+map_nside=128
+
+# X-ray observation type, determines the units of the output X-ray flux in the map.
+observation_type="photons_intrinsic"
+
+output_dir="./test_outputs/${simulation_LN}/${simulation_name}/nside${map_nside}/lightcone${lightcone_nr}_shells"
+simulation_dir="/cosma8/data/dp004/flamingo/Runs/${simulation_LN}/${simulation_name}"
+
+mkdir -p "${output_dir}" 
+
+mpirun -- python3 -m mpi4py ./examples/make_xray_map.py ${SLURM_ARRAY_TASK_ID} \
+    --nside=${map_nside} \
+    --lightcone_nr=${lightcone_nr} \
+    --simulation_dir=${simulation_dir} \
+    --output_dir=${output_dir} \
+    --xray_type=${observation_type} \
+
+
